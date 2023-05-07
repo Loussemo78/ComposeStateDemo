808a5cf360a926ddbf203ed72432a70bd94dfeea
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 16:17:34 +0200

    compose viewModel dependency added

--- app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composestatedemo.ui.theme.ComposeStateDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<MyViewModel>()
             ComposeStateDemoTheme {
                 Column(
                     modifier = Modifier.fillMaxSize(),
