043beac89ac9601a35b4cd0c3184836e53bb354c
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 15:33:19 +0200

    best practice is to put mutableStateOf in the function but with the keyword remember

--- app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,12 +39,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-val count = mutableStateOf(0)
+//val count = mutableStateOf(0)
 
 @Preview(name = "MyButton")
 @Composable
 fun MyButton(){
     val context = LocalContext.current
+    val count = remember{mutableStateOf(0)}
     Button(onClick = {
         count.value = count.value+1
         Toast.makeText(context,"Count is : ${count.value}", Toast.LENGTH_SHORT).show()
